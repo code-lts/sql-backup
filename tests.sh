82675d11c44266e23c464c46a2ba5aa632b1bbfd
William Desportes <williamdes@wdes.fr>
2018-04-21 19:17:48 +0200

    Added test cases & improved tests

--- tests.sh
+++ tests.sh
@@ -45,8 +45,8 @@ compareFilesSUM() {
 }
 
 compareFilesOrExit() {
-  #diff -ia --unified=2 --suppress-common-lines "$1" "$2"
-  compareFilesSUM "$@" || failNotSame "Files are not identical ($1) ($2)" "$(cat $1)" "$(cat $2)"
+  diff -ia --unified=1 --suppress-common-lines "$1" "$2"
+  compareFilesSUM "$@" || fail "Files are not identical ($1) ($2)"
 }
 
 testBACKUP_CONFIG_ENVFILE_Fail() {
@@ -68,65 +68,57 @@ preTest() {
   touch ./test/envfile
   echo "BACKUP_DIR=${SCRIPT_ROOT}/test" > ./test/envfile
   fillConfigFile ./test/envfile
+  export BACKUP_CONFIG_ENVFILE="./test/envfile"
 }
 
 postTest() {
   rm ./test/*
   rmdir ./test
+  unset BACKUP_CONFIG_ENVFILE
 }
 
 testBACKUP_CONFIG_ENVFILE_Success() {
   preTest
-  export BACKUP_CONFIG_ENVFILE="./test/envfile"
   echo "MYSQL_HOST=tests.test" >> "./test/envfile"
   ./backup.sh
   assertEquals 204 "$?"
-  unset BACKUP_CONFIG_ENVFILE
   postTest
 }
 
 testBACKUP_EMPTY_Success() {
   preTest
-  export BACKUP_CONFIG_ENVFILE="./test/envfile"
   ./backup.sh
   assertEquals 206 "$?"
-  unset BACKUP_CONFIG_ENVFILE
   postTest
 }
 
 testBACKUP_Success_NoDiff() {
   preTest
-  export BACKUP_CONFIG_ENVFILE="./test/envfile"
   mysql ${MYSQLCREDS} -e "CREATE DATABASE testbench CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
   ./backup.sh
   assertEquals 0 "$?"
   mysql ${MYSQLCREDS} -e "DROP DATABASE testbench;"
   compareFiles "empty"
-  unset BACKUP_CONFIG_ENVFILE
   postTest
 }
 
 testBACKUP_Success() {
   preTest
-  export BACKUP_CONFIG_ENVFILE="./test/envfile"
   mysql ${MYSQLCREDS} -e "CREATE DATABASE testbench CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
   ./backup.sh
   assertEquals 0 "$?"
   mysql ${MYSQLCREDS} -e "DROP DATABASE testbench;"
-  unset BACKUP_CONFIG_ENVFILE
   postTest
 }
 
 testOnSuccessScript() {
   preTest
-  export BACKUP_CONFIG_ENVFILE="./test/envfile"
   echo 'ON_SUCCESS="./postTest.sh"' >> "./test/envfile"
   mysql ${MYSQLCREDS} -e "CREATE DATABASE testbench CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
   ./backup.sh
   cat ./test/endfile > /dev/null
   assertEquals 0 "$?"
   mysql ${MYSQLCREDS} -e "DROP DATABASE testbench;"
-  unset BACKUP_CONFIG_ENVFILE
   postTest
 }
 
@@ -135,6 +127,33 @@ testCompareFail() {
   compareFilesSUM "${SCRIPT_ROOT}/.gitignore" "${SCRIPT_ROOT}/samples/empty/events.sql"
   assertEquals 1 "$?"
 }
+
+
+testBACKUP_Success_NoDiff_Strange_BS_Data() {
+  preTest
+  mysql ${MYSQLCREDS} -e "CREATE DATABASE testbench CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
+  mysql ${MYSQLCREDS} -e 'USE testbench;CREATE TABLE `table hérétique ! @*:` (
+    `ma première colonne` varchar(64) CHARACTER SET utf8mb4 NOT NULL,
+    `qui utilise encore du latin sérieux` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL
+  ) ENGINE=InnoDB DEFAULT CHARSET=big5 COMMENT="Une table horrible";'
+  mysql ${MYSQLCREDS} -e 'USE testbench;ALTER TABLE `table hérétique ! @*:` ADD PRIMARY KEY (`ma première colonne`);'
+  mysql ${MYSQLCREDS} -e 'USE testbench;ALTER TABLE `table hérétique ! @*:` ADD INDEX(`qui utilise encore du latin sérieux`);'
+  mysql ${MYSQLCREDS} -e 'USE testbench;ALTER TABLE `table hérétique ! @*:` ADD UNIQUE(`ma première colonne`);'
+  mysql ${MYSQLCREDS} -e 'USE testbench;ALTER TABLE `table hérétique ! @*:` ADD KEY `les cons` (`qui utilise encore du latin sérieux`) USING BTREE;'
+  ./backup.sh
+  assertEquals 0 "$?"
+  mysql ${MYSQLCREDS} -e "DROP DATABASE testbench;"
+  compareFiles "withdata"
+  postTest
+}
+
+testBACKUP_Success_NoDatabases() {
+  preTest
+  ./backup.sh
+  assertEquals 206 "$?"
+  postTest
+}
+
 
 . ./shunit2-2.1.7/shunit2
 if [ ${__shunit_testsFailed} -eq 0 ]; then
