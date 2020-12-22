ce963edb47e5866895bf8ab460776a7e25c44e4e
William Desportes <williamdes@wdes.fr>
2020-12-22 02:07:12 +0100

    Update sql_mode on events, routines, triggers of data 2

--- samples/withdata2/routines.sql
+++ samples/withdata2/routines.sql
@@ -18,7 +18,7 @@ USE `testbench`;
 /*!50003 SET character_set_results = utf8 */ ;
 /*!50003 SET collation_connection  = utf8_general_ci */ ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
 CREATE DEFINER=`testbench`@`%` FUNCTION `procédure de test`(`entrée 1` VARCHAR(35) CHARSET cp1250) RETURNS int(1) unsigned
     NO SQL
