88dcb4cc5153701eaa7dc0607090179169aa3226
William Desportes <williamdes@wdes.fr>
2020-12-22 02:40:45 +0100

    Remove the debug line

--- run-tests.sh
+++ run-tests.sh
@@ -7,5 +7,4 @@ apk add --update --no-cache mysql-client
 ./tests.sh
 kcov --include-pattern=backup.sh,tests.sh --exclude-pattern=coverage $(pwd)/coverage ./tests.sh
 
-ls -lahR ./coverage/
 mv $(pwd)/coverage/tests.sh/cov.xml $(pwd)/coverage/cov.xml
